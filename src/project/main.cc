08549a7f31710b032f737d396b352932ead866af
aur2720 <rocky.au@uleth.ca>
2023-03-26 01:49:57 -0600

    Removed tester code

--- src/project/main.cc
+++ src/project/main.cc
@@ -11,13 +11,6 @@ using namespace TransactionHelper;
 
 int main() {
 	GenDatabase* gen = new GenDatabase(false);
-	std::string i = "i";
-	int two = 2;
-	i = i  + std::to_string(two);
-	std::string i2 = "i2";
-	if (i == i2) {
-		std::cout << "good" << std::endl;
-	}
 	std::vector<std::vector<std::string>> db;
 	db = gen->genDB(100);
 	Apriorialgo* apri  = new Apriorialgo();
