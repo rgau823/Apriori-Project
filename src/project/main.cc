0626344f2cfe2c63588b9506f20a089d0df25c5d
Brian Park <brianpark999@gmail.com>
2023-03-29 19:42:53 -0600

    Uploading main.cc for testing Apriori & Idea1

--- src/project/main.cc
+++ src/project/main.cc
@@ -1,6 +1,7 @@
 #include "GenDatabase.h"
 #include "TransactionHelper.h"
 #include "Apriorialgo.h"
+#include "Idea1algo.h"
 
 using namespace TransactionHelper;
 
@@ -13,7 +14,18 @@ int main() {
 	GenDatabase* gen = new GenDatabase(false);
 	std::vector<std::vector<std::string>> db;
 	db = gen->genDB(100);
-	Apriorialgo* apri  = new Apriorialgo();
-	apri->apriori(db, 0.02);
+
+	// for (auto a : db){
+	// 	for (auto b : a)
+	// 		std::cout << b << " ";
+	// 	std::cout << std::endl;
+	// }
+
+	Idea1algo* i1  = new Idea1algo();
+	i1->idea1(db, 0.02);
+	
+	Apriorialgo* a1  = new Apriorialgo();
+	a1->apriori(db, 0.02);
+	
 	return 0;
 }
