95c5e82e0098d2e74b6faf8a05483235821d1386
Landon Constantin <landonconstantin@gmail.com>
2023-04-04 13:23:55 -0600

    added time measurements in output for both algos

--- src/project/apriori.cc
+++ src/project/apriori.cc
@@ -7,7 +7,9 @@ using namespace TransactionHelper;
 
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <chrono>
 
 int main(int argc, char *argv[]) {
 	if (argc < 3 || argc >= 4) {
@@ -27,7 +29,13 @@ int main(int argc, char *argv[]) {
 	}
 
 	Apriorialgo* a1  = new Apriorialgo();
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
 	a1->apriori(db, min_supp);
+	end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "Apriori time: " << elapsed_seconds.count() << std::setprecision(6);
+	std::cout << " seconds" << std::endl;
 	std::string suppString = std::to_string(min_supp*100);
 	suppString = suppString.substr(0, suppString.find('.')+3);
 	suppString.erase(suppString.find_last_not_of('0') + 1, std::string::npos);
--- src/project/idea1.cc
+++ src/project/idea1.cc
@@ -5,10 +5,11 @@
 
 using namespace TransactionHelper;
 
-
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <chrono>
 
 int main(int argc, char *argv[]) {
   if (argc < 3 || argc >= 4) {
@@ -28,9 +29,15 @@ int main(int argc, char *argv[]) {
 	}
 
 	Idea1algo* i1  = new Idea1algo();
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
 	i1->idea1(db, min_supp);
-
+	end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "Idea1 time: " << elapsed_seconds.count() << std::setprecision(6);
+	std::cout << " seconds" << std::endl;
 	std::string suppString = std::to_string(min_supp*100);
+	suppString = suppString.substr(0, suppString.find('.')+3);
 	suppString.erase(suppString.find_last_not_of('0') + 1, std::string::npos);
 	suppString.erase(suppString.find_last_not_of('.') + 1, std::string::npos);
 	std::string freqFilename = filename + "_Idea1_" + suppString + ".freq";
